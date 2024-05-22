5b0d2777ff26823fe98632d95376dd77d2f0a84c
Moise DIARRA <moisepr@gmail.com>
2024-05-22 11:51:58 +0000

    config EmployeeService

--- src/main/java/com/codewithproject/employee/service/EmployeeService.java
+++ src/main/java/com/codewithproject/employee/service/EmployeeService.java
@@ -1,4 +1,17 @@
 package com.codewithproject.employee.service;
 
+import com.codewithproject.employee.entity.Employee;
+import com.codewithproject.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
